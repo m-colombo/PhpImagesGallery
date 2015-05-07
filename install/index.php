07f1ee51701b3860b653e6ae350f808fd3063b7b
Michele Colombo <mr.michele.colombo@gmail.com>
2015-05-07 23:36:30 +0200

    Tested db initialization

--- install/index.php
+++ install/index.php
@@ -8,4 +8,48 @@
  */
 ?>
 
-<h2>Verifying configuration</h2>
+<h1>Installation</h1>
+
+<?php
+    require_once('install_config.php');
+
+    // Check database connection
+    $db_info = $INSTALL_CONFIG["DATABASE"];
+    $conn = new mysqli($db_info["host"], $db_info["user"], $db_info["password"], $db_info["database"]);
+
+    if ($conn->connect_error)
+        die("<h2>Database connection failed<br/>".$conn->connect_error);
+
+
+//      TODO TEST
+//    // Check libraries paths
+//    if(!file_exists($INSTALL_CONFIG["LIBRARIES_ABS_PATH"]["bootstrap"]."css/bootstrap.min.css"))
+//        die("<h2>Bootstrap not found</h2> at path: ".$INSTALL_CONFIG["LIBRARIES_ABS_PATH"]["bootstrap"]."css/bootstrap.min.css");
+//
+//    if(!file_exists($INSTALL_CONFIG["LIBRARIES_ABS_PATH"]["bootstrap"]."js/bootstrap.min.js"))
+//        die("<h2>Bootstrap not found</h2> at path: ".$INSTALL_CONFIG["LIBRARIES_ABS_PATH"]["bootstrap"]."js/bootstrap.min.js");
+//
+//    if(!file_exists($INSTALL_CONFIG["LIBRARIES_ABS_PATH"]["jquery"]))
+//        die("<h2>JQuery not found</h2> at path: ".$INSTALL_CONFIG["LIBRARIES_ABS_PATH"]["jquery"]);
+
+    // Check if tables already exists
+    foreach ($INSTALL_CONFIG["TABLES_DEFINITION"] as $key => $val) {
+        $result = $conn->query("SHOW TABLES LIKE '".($db_info["table_prefix"].$key)."'");
+
+        if ($conn->connect_error)
+            die("<h2>Database table check<br/>".$conn->connect_error);
+
+        if($result->num_rows > 0)
+            die("<h2>DUPLICATE TABLE</h2> already exists: ".($db_info["table_prefix"].$key));
+    }
+
+    // Create tables
+    foreach ($INSTALL_CONFIG["TABLES_DEFINITION"] as $key => $val) {
+        $result = $conn->query("CREATE TABLE ".($db_info["table_prefix"].$key)." ".str_replace("__TABLE__PREFIX__", $db_info["table_prefix"],$val));
+
+        if ($conn->connect_error)
+            die("<h2>Tables creation failed<br/> <b>NOT HANDLED: You have to clean by your own.</b> <br/>".$conn->connect_error);
+
+    }
+
+?>
--- install/install_config.php
+++ install/install_config.php
@@ -7,21 +7,62 @@
  * License: MIT
  */
 
-
 //TODO: FILL THIS ARRAY TO PROPERLY INSTALL PIG
+//TODO CLEAN
 $INSTALL_CONFIG = array(
 
     "DATABASE" => array(
-        "host"          => "",
-        "user"          => "",
-        "password"      => "",
-        "database"      => "",
-        "table_prefix"  => ""
+        "host"          => "localhost",
+        "user"          => "root",
+        "password"      => "root",
+        "database"      => "test",
+        "table_prefix"  => "prefix"
     ),
 
-    "LIBRARIES_PATH" => array(
-        "bootstrap" =>  "",
-        "jquery"    =>  ""
+
+    "LIBRARIES_ABS_PATH" => array(
+        "bootstrap" =>  "", // Path to the root folder, ends with directory separator /
+        "jquery"    =>  ""  // Path to jquery file.
+    ),
+
+
+    ###################################
+    #   LEAVE WHAT FOLLOWS AS IT IS   #
+    ###################################
+
+    "TABLES_DEFINITION" => array(
+        "pig_images"        => "
+            (
+                `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                `name` varchar(32),
+                `url` varchar(128) NOT NULL,
+                `width` int(11),
+                `height` int(11)
+            )
+        ",
+
+        "pig_albums"        => "
+            (
+                `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                `name` varchar(32) NOT NULL UNIQUE,
+                `description` text NOT NULL,
+                `cover` int(11) DEFAULT NULL,
+                `order` int(11) DEFAULT 0,
+                FOREIGN KEY (`cover`) REFERENCES `__TABLE__PREFIX__pig_images` (`id`) ON DELETE SET NULL
+            )",
+
+        "pig_album_images"  => "
+            (
+                `album` int(11) NOT NULL,
+                `image` int(11) NOT NULL,
+                `image_name` varchar(32) DEFAULT NULL,
+                `image_description` text,
+                FOREIGN KEY (`album`) REFERENCES `__TABLE__PREFIX__pig_albums` (`id`) ON DELETE CASCADE,
+                FOREIGN KEY (`image`) REFERENCES `__TABLE__PREFIX__pig_images` (`id`) ON DELETE CASCADE
+            )
+        "
     )
 
 );
